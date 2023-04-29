9aa93c0ab77965a2bf1840bcd7ea9a083b18c04d
sadwik159 <boulahbalmohssen.com>
2023-04-29 03:13:52 +0100

    bootstrap is working finaly

--- includes/head.php
+++ includes/head.php
@@ -1,18 +1,59 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="Mohssen Boulahbal ">
+<meta name="keywords" content="pharm gestion pyscotrop solution">
 
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="Mohssen Boulahbal ">
-	<meta name="keywords" content="pharm gestion pyscotrop solution">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
 
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+<title><?php echo $page_name;  ?></title>
 
-	<title><?php echo $page_name;  ?></title>
+<!-- Latest compiled and minified CSS -->
+<link href="assest/boot/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/datatables.min.css" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<link rel="stylesheet" type="text/css" href="css/buttons.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css">
+<!--- dashboard style-->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="shortcut icon" href="img/avatars/img1.jpg" />
+<link href="css/app.css" rel="stylesheet">
 
-	<link href="css/app.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-	
-</head>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+<!-- Latest compiled JavaScript -->
+<script type="text/javascript" src="js/jquery-3.5.1.js"></script>
+<!--script src="assest/boot/dist/js/bootstrap.bundle.min.js"></script-->
+<script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="js/buttons.print.min.js"></script>
+<script type="text/javascript" src="js/jszip.min.js"></script>
+<script type="text/javascript" src="js/pdfmake.min.js"></script>
+<script type="text/javascript" src="js/vfs_fonts.js"></script>
+<script type="text/javascript" src="js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="js/buttons.colVis.min.js"></script>
+
+<!--font Awesome-->
+<link href="assest/fontawesome/css/all.css" rel="stylesheet">
+<style type="text/css">
+	.space input {
+		margin-right: 5px;
+	}
+
+	pre {
+		font-size: 14px !important;
+	}
+
+	main,
+	table {
+		background-color: #FFFFFF !important;
+	}
+
+	.dataTables_filter {
+		display: none;
+	}
+</style>
+<script src="js/app.js"></script>
+
+<script src="js/main.js"></script>
