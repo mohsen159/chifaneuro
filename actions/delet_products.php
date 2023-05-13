b592dd656f833aa653d506fa5f6344c78cbddf11
sadwik159 <boulahbalmohssen.com>
2023-05-13 02:39:13 +0100

    the prodcut page is going well

--- actions/delet_products.php
+++ actions/delet_products.php
@@ -13,5 +13,29 @@ if (!isset($_SESSION["id"])) {
 }
 
 
+// Check if the ID parameter is provided
+if (isset($_POST['id'])) {
+  $id = $_POST['id'];
 
-?>
+  // Perform the update operation based on the ID
+  // Replace the following code with your specific update logic
+
+  // Example: Update the product with ID to have a value of 0
+  $sql = "UPDATE `prodoit` SET `amount`= 0   WHERE id = ?";
+  $stmt = $coon->prepare($sql);
+  $stmt->bind_param('i', $id);
+  $stmt->execute();
+
+  // Check if the update was successful
+  if ($stmt->affected_rows > 0) {
+    $response = array('success' => true, 'message' => 'Product updated successfully');
+  } else {
+    $response = array('success' => false, 'message' => 'Product update failed');
+  }
+
+  // Return the response as JSON
+  echo json_encode($response);
+} else {
+  $response = array('success' => false, 'message' => 'ID parameter not provided');
+  echo json_encode($response);
+}
---
+++ js/products.js
@@ -0,0 +1,252 @@
+// this part is just for your custom search
+/*$('#search').keyup(function() {
+	var table = $('#prducts').DataTable();
+	table.search($(this).val()).draw();
+});*/
+// the add model 
+// counter to track number of elements
+let counter = 1;
+
+function getProducts() {
+    const products = [];
+    $.ajax({
+        url: 'actions/get_product_list.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            for (let i = 0; i < response.length; i++) {
+                products.push({
+                    id_p: response[i].id_p,
+                    name: response[i].name,
+                    dosage: response[i].dosage,
+                    full_name: response[i].name + ' ' + response[i].dosage
+                });
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+    return products;
+}
+
+let data = getProducts();
+
+function addElement() {
+    counter++;
+    const newDiv = document.createElement('div');
+    newDiv.id = counter;
+    newDiv.className = 'mt-3 autocomplete d-flex flex-nowrap justify-content-between space';
+    newDiv.innerHTML = `
+    <input type="hidden" name="id[]">
+    <input type="text" onfocus="find_product(this)" onblur="find_productid(this)" class="form-control order-1 p-2" placeholder="Name" name="name[]" required>
+    <input type="text" class="order-2 p-2" style="width:80px"  placeholder="Lot" name="lot[]" required>
+    <input type="number"  class="order-3 p-2" style="width:100px" placeholder="Amount" name="amount[]" required>
+	<input type="date" class="order-3 p-2" style="width:220px" name="exp[]" required>
+    <li style="margin-right: 10px;" class="btn btn-danger fa fa-trash" aria-hidden="true" onclick="delet_p(this)">
+      <br>
+    </li>
+  `;
+
+    const formElement = document.getElementById('new_product');
+    const inputElement = document.getElementById('0');
+    formElement.insertBefore(newDiv, inputElement);
+
+}
+
+
+
+
+
+// function to add new input element
+function delet_p(element) {
+    if (element.parentElement === document.getElementById("1")) {
+        /// no need for the form to be visible
+        alert("this fieald can not be deleted ");
+    } else {
+        element.parentElement.remove();
+    }
+}
+/// the autocomplete 
+function find_product(element) {
+    let names = data.map(p => p.full_name);
+    autocomplete(element, names);
+}
+
+function find_productid(input) {
+    const parentDiv = input.parentNode;
+    const full_name = input.value.toLowerCase();
+    const product = data.find((p) => p.full_name.toLowerCase() === full_name);
+    if (product) {
+        const idInput = parentDiv.querySelector('input[type="hidden"]');
+        const productId = product.id_p;
+        idInput.value = productId;
+
+    } else {
+        if (full_name.length > 2) {
+            $('#add').modal('hide');
+            // this is a new product submit to product list and use the data 
+            // to update the product list show model #addproductlist
+            $('#addproductlist').modal('show'); // open the modal and fetch the input value in the name 
+            $(document).ready(function () {
+                $('#new_item').submit(function (e) {
+                    e.preventDefault(); // prevent default form submission
+
+                    // Gather form data
+                    var name = $('[name="name"]').val();
+                    var dosage = $('[name="dosage"]').val();
+                    var la_forme = $('[name="form"]').val();
+                    var dci = $('[name="dci"]').val();
+
+                    // make AJAX request
+                    $.ajax({
+                        url: 'actions/add_productslist.php', // replace with your script file
+                        type: 'POST',
+                        data: {
+                            name: name,
+                            dosage: dosage,
+                            la_forme: la_forme,
+                            dci: dci
+                        },
+                        success: function (response) {
+                            // handle success
+                            var newId = response; // ID of the new insert
+                            alert("New Product successfully inserted");
+                            input.value = name + ' ' + dosage;
+                            data = getProducts();
+                            find_productid(input);
+                            $('[name="name"]').val() = "";
+                            $('[name="dosage"]').val() = "";
+                            $('[name="form"]').val() = "";
+                            $('[name="dci"]').val() = "";
+
+                            $('#add').modal('show');
+
+
+                            // perform any additional actions with the new ID
+                        },
+                        error: function () {
+                            // handle error
+                            alert("Error occurred during insert.");
+                        }
+                    });
+                });
+            });
+        }
+
+    }
+}
+// not working yet 
+
+$(document).ready(function () {
+    $.ajax({
+        url: "actions/get_products.php",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            // Loop through the data and add each row to the table.
+            var table = $('#prducts').DataTable({
+                responsive: true,
+                paging: false,
+                dom: 'Bfrtip',
+                buttons: [{
+                        text: 'add',
+                        action: function () {
+
+                            //alert("nothing for now ")
+                            $('#add').modal('show');
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        messageTop: ' ',
+                        exportOptions: {
+                            columns: ':visible'
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        text: 'excel',
+                        exportOptions: {
+                            columns: ':visible',
+                            modifier: {
+                                page: 'current'
+                            }
+                        }
+                    },
+                    'colvis'
+                ],
+
+                columnDefs: [{
+                        targets: -1,
+                        visible: true
+                    },
+                    {
+                        targets: 0,
+                        visible: false
+                    }
+                ],
+
+                order: [
+                    [0, "desc"]
+                ]
+            });
+            for (var i = 0; i < data.length; i++) {
+                var row = [
+                    data[i]['id'],
+                    data[i]['name_dosage'],
+                    data[i]['lot'],
+                    data[i]['amount'],
+                    moment(data[i]['exp']).format('DD/MM/YYYY'),
+                    '<td><button class="delete-btn"><i class="fas fa-trash-alt"></i> Delete</button>  <button class="info-btn"><i class="fas fa-info-circle" ></i> Info</button> </td>'
+                ];
+                table.row.add(row);
+            }
+            // Draw the table to show the added data.
+            table.draw();
+            // delet button func 
+            const dbutton = document.querySelectorAll('.delete-btn');
+            dbutton.forEach(btn => {
+                btn.addEventListener('click', () => {
+
+                    if (confirm("Are you sure you want to delete this item?")) {
+
+
+                        //alert('Delete button clicked');
+                        var currentRow = btn.closest("tr");
+                        var table = $('#prducts').DataTable();
+                        var rowIdx = table.row(currentRow).index();
+                        var rowData = table.row(rowIdx).data();
+                        var id = rowData[0]; // Assuming 'id' is in the first column
+
+                        // delet the row 
+                        $.ajax({
+                            url: 'actions/delet_products.php', // Replace with the URL that handles the delete operation
+                            type: 'POST',
+                            data: {
+                                id: id
+                            }, // Pass the ID of the row to be deleted
+                            success: function (response) {
+                                // Handle the success response
+                                console.log(response);
+                                currentRow.remove();
+                            },
+                            error: function (error) {
+                                // Handle the error response
+                                console.log(error);
+                            }
+                        });
+                    } else {
+                        // User clicked "Cancel", do nothing
+                    }
+                });
+            });
+
+
+
+        },
+        error: function () {
+            alert("Error retrieving product data.");
+        }
+    });
+});
