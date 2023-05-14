11a2261da7936c0f7ac25e9614017128a93c0a4f
sadwik159 <boulahbalmohssen.com>
2023-05-14 17:43:22 +0100

    fixed

--- js/sales.js
+++ js/sales.js
@@ -46,6 +46,7 @@ var table = $('#Sales').DataTable({
 });
 /// it's the same in the prodcuts js this varibale is used there 
 let counter = 1;
+let ncounter = 1;
 
 function getProducts() {
     const products = [];
@@ -74,7 +75,7 @@ let data = getProducts();
 function addElement() {
     counter++;
     const newDiv = document.createElement('div');
-    newDiv.id = counter;
+    newDiv.id = counter + "p";
     newDiv.className = 'mt-3 autocomplete d-flex flex-nowrap justify-content-between space';
     newDiv.innerHTML = `
     <input type="hidden" name="id[]">
@@ -87,7 +88,7 @@ function addElement() {
   `;
 
     const formElement = document.getElementById('form_sales');
-    const inputElement = document.getElementById('0');
+    const inputElement = document.getElementById('complited');
     formElement.insertBefore(newDiv, inputElement);
 }
 // function to add new input element
@@ -98,6 +99,25 @@ function delet_p(element) {
     } else {
         element.parentElement.remove();
     }
+}
+/// noncmplited part of the code 
+function addnElement() {
+    ncounter++;
+    const newDiv = document.createElement('div');
+    newDiv.id = ncounter;
+    newDiv.className = 'mt-3 autocomplete d-flex flex-nowrap justify-content-between space';
+    newDiv.innerHTML = `
+    <input type="hidden" name="idn[]">
+    <input type="text" onfocus="find_product(this)" onblur="find_productid(this)" class="form-control order-1 p-2" placeholder="Name" name="namen[]" required>
+    <input type="number"  class="order-3 p-2" style="width:100px" placeholder="Amount" name="amountn[]" required>
+    <li style="margin-right: 10px;" class="btn btn-danger fa fa-trash" aria-hidden="true" onclick="delet_p(this)">
+      <br>
+    </li>
+  `;
+
+    const formElement = document.getElementById('form_sales');
+    const inputElement = document.getElementById('noncomplited');
+    formElement.insertBefore(newDiv, inputElement);
 }
 
 function find_product(element) {
@@ -172,41 +192,103 @@ function find_clientid(input) {
             $('#add').modal('hide');
             $('#addclient').modal('show');
             $('#new_client').submit(function (e) {
-                     e.preventDefault(); // prevent default form submission
-                     // Gather form data
-                     var name = $('[name="name"]').val();
-                     var fname = $('[name="fname"]').val();
-                     // make AJAX request
-                     $.ajax({
-                         url: 'actions/add_client.php', // replace with your script file
-                         type: 'POST',
-                         data: {
-                             name: name,
-                             fname: fname
-                         },
-                         success: function (response) {
-                             // handle success
-                             var newId = response; // ID of the new insert
-                             input.value = fname + ' ' + name;
-                             $('[name="name"]').val("");;
-                             $('[name="name"]').val("");;
-                             $('#addclient').modal('hide');
-                             clients = getclients();
-                             idInput.value = newId;
-                             $('#add').modal('show');
+                e.preventDefault(); // prevent default form submission
+                // Gather form data
+                var name = $('[name="name"]').val();
+                var fname = $('[name="fname"]').val();
+                // make AJAX request
+                $.ajax({
+                    url: 'actions/add_client.php', // replace with your script file
+                    type: 'POST',
+                    data: {
+                        name: name,
+                        fname: fname
+                    },
+                    success: function (response) {
+                        // handle success
+                        var newId = response; // ID of the new insert
+                        input.value = fname + ' ' + name;
+                        $('[name="name"]').val("");;
+                        $('[name="name"]').val("");;
+                        $('#addclient').modal('hide');
+                        clients = getclients();
+                        idInput.value = newId;
+                        $('#add').modal('show');
 
-                             /// find_clientid(input);   /// stupid method *
+                        /// find_clientid(input);   /// stupid method *
 
 
-                             // perform any additional actions with the new ID
-                         },
-                         error: function () {
-                             // handle error
-                             alert("Error occurred during insert.");
-                         }
-                     });
-                 });
+                        // perform any additional actions with the new ID
+                    },
+                    error: function () {
+                        // handle error
+                        alert("Error occurred during insert.");
+                    }
+                });
+            });
         }
     }
 
 }
+$('#form_sales').submit(function (e) {
+    e.preventDefault(); // prevent default form submission
+
+    // Retrieve form values
+    var client = $('#input_client').val();
+    var employee = $('#select').val();
+    var products = [];
+
+    $('.autocomplete').each(function () {
+        var productName = $(this).find('input[name="name[]"]').val();
+        var lot = $(this).find('input[name="lot[]"]').val();
+        var amount = $(this).find('input[name="amount[]"]').val();
+
+        // Create an object for each product
+        var product = {
+            name: productName,
+            lot: lot,
+            amount: amount
+        };
+
+        products.push(product);
+    });
+
+    var noncomplitedProducts = [];
+
+    $('.autocomplete-n').each(function () {
+        var productName = $(this).find('input[name="namen[]"]').val();
+        var amount = $(this).find('input[name="amountn[]"]').val();
+
+        // Create an object for each noncomplited product
+        var product = {
+            name: productName,
+            amount: amount
+        };
+
+        noncomplitedProducts.push(product);
+    });
+
+    var orderNumber = $('#num_order').val();
+    var saleDate = $('#sale_date').val();
+    var duration = $('#dure').val();
+    var note = $('#note').val();
+
+    // Create an object to hold all the form data
+    var formData = {
+        client: client,
+        employee: employee,
+        products: products,
+        noncomplitedProducts: noncomplitedProducts,
+        orderNumber: orderNumber,
+        saleDate: saleDate,
+        duration: duration,
+        note: note
+    };
+
+    // Display the form data in an alert
+    alert(JSON.stringify(formData));
+
+    // Proceed with form submission
+    // Uncomment the following line to submit the form
+    // $(this).unbind('submit').submit();
+});
--- model/sales_model.php
+++ model/sales_model.php
@@ -11,18 +11,15 @@
 
             <!-- Modal body -->
             <div class="modal-body">
-                <form autocomplete="off" action="actions/insert_sale.php" method="post" class="was-validated"
-                    id="form_sales" name="form_sales">
+                <form autocomplete="off" action="actions/insert_sale.php" method="post" class="was-validated" id="form_sales" name="form_sales">
                     <div class="mb-3 mt-3 autocomplete">
                         <input type="hidden" name="clientid">
-                        <input type="text" onfocus="find_client(this)" onblur="find_clientid(this)" class="form-control"
-                            id="input_client" placeholder="client" name="client" required>
+                        <input type="text" onfocus="find_client(this)" onblur="find_clientid(this)" class="form-control" id="input_client" placeholder="client" name="client" required>
                         <div class="valid-feedback">Valid.</div>
                         <div class="invalid-feedback">Please fill out this field.</div>
                     </div>
                     <br>
-                    <select id="select" class="form-select form-select-sm" aria-label="Default select example"
-                        name="employs">
+                    <select id="select" class="form-select form-select-sm" aria-label="Default select example" name="employs">
                         <!--fetch user -->
                         <?php
                         // Assuming you have a database connection established
@@ -58,29 +55,30 @@
 
                     <div id="1" class="mt-3 autocomplete d-flex flex-nowrap justify-content-between space">
                         <input type="hidden" name="id[]">
-                        <input type="text" onfocus="find_product(this)" onblur="find_productid(this)"
-                            class="form-control order-1 p-2" placeholder="Name" name="name[]" required>
-                        <input type="text" class="order-2 p-2" style="width:80px" placeholder="Lot" name="lot[]"
-                            required>
-                        <input type="number" class="order-3 p-2" style="width:100px" placeholder="Amount"
-                            name="amount[]" required>
-                        <li style="margin-right: 10px;" class="btn btn-danger fa fa-trash" aria-hidden="true"
-                            onclick=" delet_p(this)">
+                        <input type="text" onfocus="find_product(this)" onblur="find_productid(this)" class="form-control order-1 p-2" placeholder="Name" name="name[]" required>
+                        <input type="text" class="order-2 p-2" style="width:80px" placeholder="Lot" name="lot[]" required>
+                        <input type="number" class="order-3 p-2" style="width:100px" placeholder="Amount" name="amount[]" required>
+                        <li style="margin-right: 10px;" class="btn btn-danger fa fa-trash" aria-hidden="true" onclick=" delet_p(this)">
                             <br>
                     </div>
                     <!-- don't delet this input we use it to add new div before  -->
-                    <input type="hidden" id="0">
+                    <input type="hidden" id="complited">
                     <br>
-                    <button style="margin-top:10px" type="button" onclick="addElement()"
-                        class="fa fa-plus btn btn-primary"></button>
+                    <button style="margin-bottom:30px" type="button" onclick="addElement()" class="fa fa-plus btn btn-primary"></button>
+                    <!-- noncomplited part -->
+
+                    <!-- don't delet this input we use it to add new div before  -->
+                    <input type="hidden" id="noncomplited">
+                    <br>
+                    <button style="margin-top:10px" type="button" onclick="addnElement()" class="fa fa-plus btn btn-primary">Add noncomplited</button>
+                    <!-- num order  -->
                     <div class="mb-3 mt-3">
-                        <input type="text" class="form-control" id="num_order" placeholder="num_order" name="num_order"
-                            required>
+                        <input type="text" class="form-control" id="num_order" placeholder="num_order" name="num_order" required>
                         <div class="valid-feedback">Valid.</div>
                         <div class="invalid-feedback">Please fill out this field.</div>
                     </div>
                     <div class="mb-3">
-                        <label for="sale_date" class="form-label">sale date</label>
+                        <label for="sale_date" class="form-label">Sale date</label>
                         <input type="date" id="sale_date" name="sale_date" required>
                         <div class="valid-feedback">Valid.</div>
                         <div class="invalid-feedback">Enter a valid date.</div>
@@ -104,6 +102,11 @@
     </div>
 
 </div>
+
+
+
+
+
 <!--edit-->
 <div class="modal fade bd-example-modal-sm" id="edit">
     <div class="modal-dialog">
@@ -117,23 +120,20 @@
 
             <!-- Modal body -->
             <div class="modal-body">
-                <form autocomplete="off" action="/operation/update_sale.php" method="post" class="was-validated"
-                    id="sale_update" name="sale_update">
+                <form autocomplete="off" action="/operation/update_sale.php" method="post" class="was-validated" id="sale_update" name="sale_update">
                     <div class="mb-3 mt-3">
                         <input type="hidden" class="form-control" id="id" name="id" required>
                         <!-- this on soo we can know wich sales id -->
                     </div>
                     <div class="mb-3 mt-3">
-                        <textarea class="form-control" id="medication" placeholder="medication" name="medication"
-                            required>
+                        <textarea class="form-control" id="medication" placeholder="medication" name="medication" required>
                                </textarea>
                         <div class="valid-feedback">Valid.</div>
                         <div class="invalid-feedback">Please fill out this field.</div>
                     </div>
 
                     <div class="mb-3 mt-3">
-                        <input type="text" class="form-control" id="num_order" placeholder="num_order" name="num_order"
-                            required>
+                        <input type="text" class="form-control" id="num_order" placeholder="num_order" name="num_order" required>
                         <div class="valid-feedback">Valid.</div>
                         <div class="invalid-feedback">Please fill out this field.</div>
                     </div>
@@ -145,8 +145,7 @@
                     </div>
                     <br>
                     <div class="mb-3">
-                        <select id="select" class="form-select form-select-sm" aria-label="Default select example"
-                            name="employs">
+                        <select id="select" class="form-select form-select-sm" aria-label="Default select example" name="employs">
                             <option value="1">wissam</option>
                             <option value="2">mohssen</option>
                             <option value="3">hakim</option>
@@ -158,8 +157,7 @@
                     </div>
                     <br>
                     <div class="mb-3 mt-3">
-                        <input type="text" min="1" max="100" class="form-control" id="dure" placeholder="dure"
-                            name="dure" required>
+                        <input type="text" min="1" max="100" class="form-control" id="dure" placeholder="dure" name="dure" required>
                         <div class="valid-feedback">Valid.</div>
                         <div class="invalid-feedback">Please fill out this field.</div>
                     </div>
@@ -170,9 +168,7 @@
                         <div class="invalid-feedback">Please fill out this field.</div>
                     </div>
                     <button type="submit" class="btn btn-outline-success">Edit</button>
-                    <button type="submit" class="btn btn-outline-danger"
-                        onclick='delet( document.forms["sale_update"]["id"].value ,"sales")'
-                        style="float:right">Delete</button>
+                    <button type="submit" class="btn btn-outline-danger" onclick='delet( document.forms["sale_update"]["id"].value ,"sales")' style="float:right">Delete</button>
                 </form>
 
             </div>
@@ -185,7 +181,6 @@
 
 
 <!--add new client -->
-
 <div class="modal" id="addclient">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -205,8 +200,7 @@
                         <div class="invalid-feedback">Please fill out this field.</div>
                     </div>
                     <div class="mb-3 mt-3">
-                        <input type="text" class="form-control" id="fname" placeholder="Enter family name" name="fname"
-                            required>
+                        <input type="text" class="form-control" id="fname" placeholder="Enter family name" name="fname" required>
                         <div class="valid-feedback">Valid.</div>
                         <div class="invalid-feedback">Please fill out this field.</div>
                     </div>
@@ -214,9 +208,6 @@
                 </form>
 
             </div>
-
-
-
         </div>
     </div>
 
