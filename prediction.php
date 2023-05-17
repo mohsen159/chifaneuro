ecd907daeb836350093e2e3702bff08c1bcb42f5
sadwik159 <boulahbalmohssen.com>
2023-05-17 02:41:02 +0100

    prediction is working but not deleting inventory we need more data to make the predction right

--- prediction.php
+++ prediction.php
@@ -18,43 +18,29 @@ $start_date = date("Y-m-d");
 $end_date = date('Y-m-d', strtotime($start_date . ' + 1 month')); // Adjust the interval as needed
 
 // Query the database to find the products purchased in potential orders within the time period
-$sql = "SELECT p.id, lp.id AS list_id, lp.name, lp.dosage, SUM(c.amount) AS total_amount
-        FROM `changement` c
-        INNER JOIN prodoit p ON c.id_prodoit = p.id
-        INNER JOIN list_prodoit lp ON p.list_prodoit = lp.id
-        WHERE c.id_ord IN (
+$sql = "SELECT id_prodoit, SUM(amount) AS total_amount
+        FROM `changement`
+        WHERE id_ord IN (
             SELECT id
             FROM `ord`
             WHERE next_date >= '$start_date' AND next_date < '$end_date'
         )
-        GROUP BY p.id";
+        GROUP BY id_prodoit";
 $result = mysqli_query($coon, $sql);
 
 // Create an array to store the predicted products and their quantities
 $predicted_products = array();
 while ($row = mysqli_fetch_assoc($result)) {
-    $product_id = $row['id'];
-    $list_product_id = $row['list_id'];
-    $product_name = $row['name'];
-    $product_dosage = $row['dosage'];
+    $product_id = $row['id_prodoit'];
     $amount = $row['total_amount'];
-    $predicted_products[$product_id] = array(
-        'list_id' => $list_product_id,
-        'name' => $product_name,
-        'dosage' => $product_dosage,
-        'quantity' => $amount
-    );
+    $predicted_products[$product_id] = $amount;
 }
 
 // Display the predicted products needed
 if (!empty($predicted_products)) {
     echo "Predicted products needed within the period from $start_date to $end_date:<br>";
-    foreach ($predicted_products as $product_id => $product_data) {
-        $list_product_id = $product_data['list_id'];
-        $product_name = $product_data['name'];
-        $product_dosage = $product_data['dosage'];
-        $quantity = $product_data['quantity'];
-        echo "Product ID: $product_id, List Product ID: $list_product_id, Name: $product_name, Dosage: $product_dosage, Quantity: $quantity<br>";
+    foreach ($predicted_products as $product_id => $amount) {
+        echo "Product ID: $product_id, Quantity: $amount<br>";
     }
 } else {
     echo "No predicted products needed within the period from $start_date to $end_date.";
